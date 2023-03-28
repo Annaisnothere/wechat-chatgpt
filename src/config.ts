d684296214e9868f2e8a7f2cf6de45bd57a496c7
a13707621@gmail.com <a13707621@gmail.com>
2023-03-28 18:26:20 +0800

    remove default prompt

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const config: IConfig = {
   chatPrivateTiggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
   chatTiggerRule: process.env.CHAT_TRIGGER_RULE || "",
   disableGroupMessage: process.env.DISABLE_GROUP_MESSAGE === "true",
-  prompt: process.env.PROMPT || "You are a kind human.",
+  prompt: process.env.PROMPT || "",
   blockedNames: process.env.BLOCKED_NAMES?.split(",") || [],
   max_tokens: parseInt(process.env.MAX_TOKENS || "100"),
 };
