5c6789f8fdd3294768d5ca1b0e133baa1c507246
a13707621@gmail.com <a13707621@gmail.com>
2023-03-30 00:51:29 +0800

    swith timer to wechaty7

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { WechatyBuilder } from "wechaty";
 import { Wechaty, Message } from 'wechaty';
 import QRCode from "qrcode";
 import { ChatGPTBot } from "./bot.js";
-const chatGPTBot = new ChatGPTBot();
 
-const bot =  WechatyBuilder.build({
+const chatGPTBot = new ChatGPTBot();
+const messageCache = new Map();
+const bot = WechatyBuilder.build({
   name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
 });
 async function main() {
-  const initializedAt = Date.now()
+  const initializedAt = Date.now();
   bot
     .on("scan", async (qrcode, status) => {
       const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
@@ -30,27 +31,11 @@ async function main() {
         return;
       }
       try {
-        await chatGPTBot.onMessage(message);
+        await handleMessage(message);
       } catch (e) {
         console.error(e);
       }
     });
-
-  async function clearOldMessages(bot: Wechaty, seconds: number) {
-    const messages = await bot.Message.findAll();
-    const cutoffTime = new Date(Date.now() - seconds * 1000);
-    
-    for (const message of messages) {
-      if (message.date() < cutoffTime) {
-        await message.del();
-        console.log(`Message "${message.text()}" has been cleared.`);
-      }
-    }
-  }
-  // Clear old messages every 3 minutes
-  setInterval(async () => {
-    await clearOldMessages(bot, 180); // 180 seconds = 3 minutes
-  }, 3 * 60 * 1000); //  3 minutes
 
   try {
     await bot.start();
@@ -60,4 +45,27 @@ async function main() {
     );
   }
 }
+
+async function handleMessage(message: Message) {
+  const messageId = message.id;
+  const messageDate = message.date();
+  messageCache.set(messageId, messageDate);
+
+  // Handle message with ChatGPTBot
+  await chatGPTBot.onMessage(message);
+
+  clearOldMessagesFromCache(180);
+}
+
+function clearOldMessagesFromCache(seconds: number) {
+  const cutoffTime = new Date(Date.now() - seconds * 1000);
+
+  for (const [messageId, messageDate] of messageCache.entries()) {
+    if (messageDate < cutoffTime) {
+      messageCache.delete(messageId);
+      console.log(`Message "${messageId}" has been cleared from cache.`);
+    }
+  }
+}
+
 main();
